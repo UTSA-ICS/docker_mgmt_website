3ca7956e430bfed3657024cb0e06dd0aaddf2a9b
James Benson <JamesOBenson@users.noreply.github.com>
2020-08-04 16:11:09 -0500

    Update start_website.sh

--- start_website.sh
+++ start_website.sh
@@ -4,5 +4,6 @@ if [ -f "ics_website" ]; then
     chmod 600 ics_website
 else echo "ics_website not found"
     exit 1
+fi
 
 GIT_SSH_COMMAND='ssh -i /ics_website' git clone git@github.com:UTSA-ICS/ICS-Admin.git
